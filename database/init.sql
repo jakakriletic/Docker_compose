1de8b5b5af9d4a41b7142291db3ec1eebb9272e2
jaka <jakakriletic1@gmail.com>
2026-06-04 16:13:30 +0000

    premik na windows

---
+++ database/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE test_baza;
+
+CREATE TABLE company (
+    company_name varchar(255) PRIMARY KEY,
+    ceo varchar(255),
+    founded_year varchar(255),
+    industry varchar(255),
+    main_activity varchar(255)
+);
