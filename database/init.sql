81a067a6147d1b1057a733a1758a9f90a77fd07c
jakakriletic <jakakriletic1@gmail.com>
2026-06-04 18:56:27 +0200

    11

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE test_baza;
-
+USE test_baza;
 CREATE TABLE company (
     company_name varchar(255) PRIMARY KEY,
     ceo varchar(255),
-    founded_year varchar(255),
+    founded_year int,
     industry varchar(255),
     main_activity varchar(255)
 );
