d781f7a5117e6b86fffe08adb5b41aed3906f80e
jakakriletic <jakakriletic1@gmail.com>
2026-06-05 11:57:23 +0200

    jajaja

--- post_client/post.py
+++ post_client/post.py
@@ -2,17 +2,13 @@ import httpx
 import json
 
 
-def start_app():
-    with open("companies.json","r" , encoding="utf-8") as file:
-        companies = json.load(file)
+with open("companies.json","r" , encoding="utf-8") as file:
+    companies = json.load(file)
 
-    for company in companies:
-        r = httpx.post("http://api:80/company/postdata", json=company)
+for company in companies:
+    r = httpx.post("http://api:80/company/postdata", json=company)
 
 
-input("1 za nadeljevanje")
 
-if input == 1:
-    start_app()
   
 
